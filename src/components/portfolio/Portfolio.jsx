382b8a4c3238ff61d46261de681a7864c5b49ad0
Sezam <dossumbekov@list.ru>
2022-08-13 23:43:46 +0600

    fix: portfolio img

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,33 +1,31 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../assets/portfolio1.jpg";
-import IMG2 from "../../assets/portfolio2.jpg";
-import IMG3 from "../../assets/portfolio3.jpg";
-import IMG4 from "../../assets/portfolio4.jpg";
-import IMG5 from "../../assets/portfolio5.png";
-import IMG6 from "../../assets/portfolio6.jpg";
+import IMG1 from "../../assets/portfolio1.png";
+import IMG2 from "../../assets/porfolio2.png";
+import IMG3 from "../../assets/portfolio3.png";
+
 
 const data =[
   {
     id:1,
     image: IMG1,
-    title: 'Chat App using React and Socket',
-    github: 'https://github.com/anantraghuvanshi/chat-app-using-react-socketIO',
-    demo: 'https://chat-infinity.netlify.app/'
+    title: 'Movies search',
+    github: 'https://github.com/sezamiti/Starter-project-with-api',
+    demo: 'https://sezamiti.github.io/Starter-project-with-api/'
   },
   {
     id:2,
     image: IMG2,
-    title: 'Chat App using React and Socket',
-    github: 'https://github.com/anantraghuvanshi/chat-app-using-react-socketIO',
-    demo: 'https://chat-infinity.netlify.app/'
+    title: 'Employees',
+    github: 'https://github.com/sezamiti/employees',
+    demo: 'https://employees-nu.vercel.app/'
   },
   {
     id:3,
     image: IMG3,
-    title: 'Chat App using React and Socket',
-    github: 'https://github.com/anantraghuvanshi/chat-app-using-react-socketIO',
-    demo: 'https://chat-infinity.netlify.app/'
+    title: 'Blog-Q-A(todo list)',
+    github: 'https://github.com/sezamiti/Blog-Q-A',
+    demo: 'https://my-project-1-943c9.web.app/'
   },
   
 
@@ -49,7 +47,7 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={image} alt={title} />
           </div>
-          <h3>Chat App using React and Socket</h3>
+          <h3>{title}</h3>
           <div className="portfolio__item-cta">
             <a
               href={github}
