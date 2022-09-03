b7b821611969514b1fd4a2f77543f44f046d1b32
Sezam <dossumbekov@list.ru>
2022-09-04 02:15:40 +0600

    fix: cv2

--- src/components/aboutSide/About.jsx
+++ src/components/aboutSide/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src={ME} alt="Anant About" />
+            <img src={ME} alt="About" />
           </div>
         </div>
 
