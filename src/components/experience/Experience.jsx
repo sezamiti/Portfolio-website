9fb6fbb07e760205d3b5cb185197888b206b2cb2
Sezam <dossumbekov@list.ru>
2022-12-27 18:08:57 +0600

    fix: experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -56,7 +56,7 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className= 'experience__details-icon' />
               <div>
-                <h4>Node.js</h4>
+                <h4>Laravel</h4>
                 <small className="text-light">Intermediate</small>
               </div>
             </article>
@@ -71,6 +71,13 @@ const Experience = () => {
               <BsPatchCheckFill className= 'experience__details-icon' />
               <div>
                 <h4>Git/Github</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className= 'experience__details-icon' />
+              <div>
+                <h4>Docker</h4>
                 <small className="text-light">Intermediate</small>
               </div>
             </article>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -110,4 +110,5 @@ const Portfolio = () => {
   );
 };
 
+
 export default Portfolio;
